c4f71b258b2488d34b5636526c0fbec1689cf8d6
chantellalvarenga <chantell.alvarenga@gmail.com>
2021-05-09 21:44:06 -0600

    Resolucion guia 07

---
+++ GUIA07/LoginFirebase/app/src/main/java/sv/edu/udb/loginfirebase/MainActivity.java
@@ -0,0 +1,123 @@
+package sv.edu.udb.loginfirebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthCredential;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 200;
+    private static final String TAG = "MyActivity" ;
+    private EditText emailTV, passwordTV;
+    Button registerBtn, loginBtn, googleloginBtn;
+    private ProgressBar progressBar;
+
+
+
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        initializeViews();
+
+
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUserAccount();
+            }
+        });
+
+
+    }
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    //inicio de sesion con contraseña y correo
+    private void loginUserAccount() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        String email, password;
+        email = emailTV.getText().toString();
+        password = passwordTV.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+
+                            Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    private void initializeViews() {
+        emailTV = findViewById(R.id.email);
+        passwordTV = findViewById(R.id.password);
+        registerBtn = findViewById(R.id.register);
+        loginBtn = findViewById(R.id.login);
+        progressBar = findViewById(R.id.progressBar);
+
+
+
+    }
+}
